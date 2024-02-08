1d09cf9731795d8590b9a465ae448c65828311d1
Georg <https://github.com/GeorgeBrat/helloworld.git>
2024-02-08 23:37:42 +0300

    Исправил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,19 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
         // Task2
-        var a = 4;
-        System.out.println(dog + a);
-        System.out.println(cat + a);
-        System.out.println(paper + a);
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
         // Task3
-        var b = 3.5;
-        System.out.println(dog - b);
-        var c = 1.6;
-        System.out.println(cat - c);
-        var d = 7639;
-        System.out.println(paper - d);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
         // Task 4
         var friend = 19;
         System.out.println(friend);
@@ -44,7 +46,7 @@ public class Main {
         System.out.println(diff + "кг. разница между массами бойцов");
         // Task 7
         var remainder = boxer2 % boxer1;
-        System.out.println(remainder + "остаток от деления");
+        System.out.println(remainder + " остаток от деления");
         // Task 8.1
         var workTime = 640;
         var oneWorkerTime = 8;
@@ -52,7 +54,7 @@ public class Main {
         System.out.println("Всего работников в компании — " + quantityWorkers + " человек");
         // Task 8.2
         quantityWorkers = quantityWorkers + 94;
-        oneWorkerTime = workTime / quantityWorkers;
-        System.out.println("Если в компании работает " + quantityWorkers + " человек, то всего " + oneWorkerTime + " часов работы может быть поделено между сотрудниками».");
+        workTime = quantityWorkers * oneWorkerTime;
+        System.out.println("Если в компании работает " + quantityWorkers + " человек, то всего " + workTime + " часов работы может быть поделено между сотрудниками».");
     }
 }
